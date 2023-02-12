ff8c9c4bfad98d0990294ae1c5a772268cb5deec
future <future@DESKTOP-FJE1GN6>
2023-02-12 23:12:18 +0200

    Add project files.

---
+++ MultipleSclerosis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultipleSclerosis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
